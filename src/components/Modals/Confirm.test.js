3b3047ae9e127596534d63e3c39624a69cff9ad5
burczu <b.dybowski@gmail.com>
2018-01-15 20:24:02 +0100

    tests for Confirm component

--- src/components/Modals/Confirm.test.js
+++ src/components/Modals/Confirm.test.js
@@ -24,4 +24,76 @@ describe('Confirm component', () => {
       expect(wrapper.find('div.container--visible')).to.have.length(1);
     });
   });
+
+  describe('if sets "isVisible" prop to "false"', () => {
+    it('it doesn\'t show the container', () => {
+      const wrapper = Enzyme.shallow(
+        <Confirm isVisible={false} onCancelClick={() => {}} onConfirmClick={() => {}} question="test"/>
+      );
+
+      expect(wrapper.find('div.container--visible')).to.have.length(0);
+    })
+  });
+
+  describe('if "header" prop is not set', () => {
+    it('it pass default header to the ResponsivePanel component', () => {
+      const wrapper = Enzyme.shallow(
+        <Confirm isVisible onCancelClick={() => {}} onConfirmClick={() => {}} question="test" />
+      );
+
+      expect(wrapper.find(ResponsivePanel).prop('header')).to.be.eql('Prośba o potwierdzenie');
+    });
+  });
+
+  describe('if "header" prop is set', () => {
+    it('it pass this value to the ResponsivePanel component', () => {
+      const wrapper = Enzyme.shallow(
+        <Confirm isVisible onCancelClick={() => {}} onConfirmClick={() => {}} question="test" header="test header" />
+      );
+
+      expect(wrapper.find(ResponsivePanel).prop('header')).to.be.eql('test header');
+    });
+  });
+
+  describe('if "Cancel" button clicked', () => {
+    const handlers = { cancel: () => {}, confirm: () => {} };
+    let cancelClickSpy;
+    beforeEach(() => {
+      cancelClickSpy = sinon.spy(handlers, 'cancel');
+    });
+
+    afterEach(() => {
+      cancelClickSpy.restore();
+    });
+
+    it('it calls the "onCancelClick" event handler', () => {
+      const wrapper = Enzyme.shallow(
+        <Confirm isVisible onCancelClick={handlers.cancel} onConfirmClick={handlers.confirm} question="test" />
+      );
+
+      wrapper.find('button.panel__button--cancel').simulate('click');
+      expect(cancelClickSpy).to.be.called;
+    });
+  });
+
+  describe('if "Confirm" button clicked', () => {
+    const handlers = { cancel: () => {}, confirm: () => {} };
+    let confirmClickSpy;
+    beforeEach(() => {
+      confirmClickSpy = sinon.spy(handlers, 'confirm');
+    });
+
+    afterEach(() => {
+      confirmClickSpy.restore();
+    });
+
+    it('it calls the "onConfirmClick" event handler', () => {
+      const wrapper = Enzyme.shallow(
+        <Confirm isVisible onCancelClick={handlers.cancel} onConfirmClick={handlers.confirm} question="test" />
+      );
+
+      wrapper.find('div.panel__buttons').childAt(1).simulate('click');
+      expect(confirmClickSpy).to.be.called;
+    });
+  });
 });
