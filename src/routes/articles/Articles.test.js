b9ca8d5876e4dc82fe8906a4c41727591603d870
burczu <b.dybowski@gmail.com>
2018-05-31 16:24:14 +0200

    snapshot test for articles added

---
+++ src/routes/articles/Articles.test.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import React from 'react';
+import Articles from './Articles';
+import renderer from 'react-test-renderer';
+import StyleProvider from '../../../test/StyleProvider';
+import configureStore from 'redux-mock-store';
+
+describe('Articles component', () => {
+  const mockBlog = {
+    name: 'mock name',
+    favicon: 'mock favicon',
+    href: 'mock href',
+    date: 'mock date'
+  };
+  const mockArticle = {
+    title: 'mock title',
+    description: 'mock description',
+    blog: mockBlog,
+    href: 'mock href'
+  };
+  const mockState = {
+    article: mockArticle,
+    articleLoading: false
+  };
+
+  const mockStore = configureStore([]);
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <StyleProvider insertCss={() => {}}>
+          <Articles articlesState={mockState} context={{ path: 'mock path' }} store={mockStore(mockState)} />
+        </StyleProvider>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
